1dbee7b46ac41170f35c4c4402f033a86012a734
MarcusGarfunkel <MarcusGarfunkel@users.noreply.github.com>
2026-03-16 15:32:06 -0700

    Change base path in Vite config to root

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/westcoast-movers-hub/",
+  //base: "/westcoast-movers-hub/",
+  base: "/",
   
   server: {
     host: "::",
